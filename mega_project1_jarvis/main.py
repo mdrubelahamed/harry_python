fa23b64f65c62fb2616eac980594c9ac8d22ea71
Rubel <work.mdrubelahamed@gmail.com>
2025-05-10 08:50:55 -0700

    added new speak function using gTTS and openai just left

--- mega_project1_jarvis/main.py
+++ mega_project1_jarvis/main.py
@@ -1,18 +1,44 @@
+import os
 import webbrowser
 import pyttsx3
 import speech_recognition as sr
 import music_library
 import website_library
 import requests
+import pygame
+from dotenv import load_dotenv
+from gtts import gTTS
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-api_key = "3583ef69bb974afa8bf0837ad189bc8b"
+
+load_dotenv()  # loads from .env
+api_key = os.getenv("API_KEY")
 
 # speak what the text he will get
-def speak(text):
+def speak_old(text):
     engine.say(text)
     engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3')
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
 
 
 # it will process the command and find if any command can get executed
@@ -22,23 +48,21 @@ def processCommand(c):
     elif "open youtube" in c.lower():
         webbrowser.open("https://youtube.com")
 
-    # play music
     elif c.lower().startswith("play"):
         word = c.lower().split(" ")[1]
         if word in music_library.music:
             link = music_library.music[word]
             webbrowser.open(link)
-    
-    # open websites
+
     elif c.lower().startswith("open"):
         website = c.lower().split(" ")[1]
         if website in website_library.websites:
             link = website_library.websites[website]
             webbrowser.open(link)
 
-    # get news
     elif "news" in c.lower():
-        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}")
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}")
+        print(r)
         if r.status_code == 200:
             # Parse the JSON response
             data = r.json()
@@ -49,7 +73,9 @@ def processCommand(c):
             # Print the headlines
             for article in articles:
                 speak(article['title'])
-
+    else:
+        # add openai key, not added right now because of some banking problem will add later
+        pass
 
 # start main 
 if __name__ == "__main__":
---
+++ mega_project1_jarvis/musicLibrary.py
@@ -0,0 +1,19 @@
+music = {
+    "love": "https://www.youtube.com/watch?v=w0IqafBldLo",
+    "dance": "https://www.youtube.com/watch?v=XkUjEl4HqpI",
+    "party": "https://www.youtube.com/watch?v=XkUjEl4HqpI",
+    "dose": "https://youtu.be/TvngY4unjn4?si=-ELSk1-8Gj0VgobS",
+    "slow" : "https://youtu.be/nCD2hj6zJEc?si=H-asACXcBC6z3DO-",
+    "sunny": "https://youtu.be/MXJCnccDLA0?si=ifw9c5nuwvlHgQf_",
+    "brown": "https://youtu.be/eizmCZv3aKI?si=-CxkDg80WSB4guT0",
+    "regret": "https://www.youtube.com/watch?v=PVxc5mIHVuQ",
+    "angel": "https://youtu.be/KGn-erOG-Bs?si=OFrcZ-QP17Ozihq3",
+    "thunder": "https://www.youtube.com/watch?v=HP2zqQsrsyg",
+    "red angel": "https://www.youtube.com/watch?v=KGn-erOG-Bs",
+    "honey list": "https://www.youtube.com/watch?v=vgKdf8ktVtU",
+    "repent": "https://www.youtube.com/watch?v=LK7-_dgAVQE&t=20",
+    "angel": "https://www.youtube.com/watch?v=KGn-erOG-Bs",
+    "regret": "https://youtu.be/PVxc5mIHVuQ?si=PgZd0JZbvADbADw-&t=30",
+    "harry python": "https://www.youtube.com/watch?v=UrsmFxEIp5k&t=34232`", 
+}
+
